c1bae064b97dca7f7d6fea8e76ff586fb446cfdb
guillermo varela <guillovarela16@gmail.com>
2021-08-12 13:42:05 -0300

    Add review model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :services
+  resources :services do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
