41b1c232c7ed6ad61fd8cb1efb730c821de43175
daniel vega <daniel7491@gmail.com>
2021-08-19 11:13:18 -0400

    subiendo show service

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -26,7 +26,7 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :address_client)
   end
 
   def set_booking
