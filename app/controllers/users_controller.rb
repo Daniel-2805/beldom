76d75737573cb74f7f7c91d4b8aecc129b0da51b
Sergio Quiroga <squiroga.on@gmail.com>
2021-08-25 11:08:21 -0400

    Features ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @services = Service.all
+    @booking = Booking.all
     authorize @user
   end
 end
