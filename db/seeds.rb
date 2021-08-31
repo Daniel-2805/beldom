9942142120abdd1ba16a2a6ca1974b79b5040b40
carolina tordecilla <c.tordecilla.diaz@gmail.com>
2021-08-31 10:52:49 -0400

    botones listos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destruyendo archivos"
+Booking.destroy_all
+Service.destroy_all
+User.destroy_all
+
+puts "creando usuarios"
+
+User.create email: "admin@mail.com", password: "123456", permission_level: 2
+User.create email: "owner@mail.com", password: "123456", permission_level: 1
+User.create email: "caro@mail.com", password: "123456", permission_level: 0
+
+puts "finish"
