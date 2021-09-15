d4851ab77e38ac832a68b0f0732ad4354ff2ed71
daniel vega <daniel7491@gmail.com>
2021-09-14 21:27:31 -0300

    subiendo pago

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,8 @@ class UsersController < ApplicationController
       { lat: booking.latitude, lng: booking.longitude }
     end
     authorize @user
-
   end
-
-  def search
-    @user = User.find(params[:id])
-    @services = Service.all
-    @booking = Booking.near(params[:query],10)
-    
-    @markers= @booking.map do |booking|
-      { lat: booking.latitude, lng: booking.longitude }
-    end
-    authorize @user
-
+  
   def admin
     @user = User.find(params[:id])
     @user.permission_level = 2
